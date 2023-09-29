7ee3ec94807e583ec58baa407bd1bce5c92507ac
TheTubbyRaider <grsraider66@gmail.com>
2023-09-28 22:12:48 -0600

    week3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();        // 1/1
+        Fraction fraction2 = new Fraction(5);       // 5/1
+        Fraction fraction3 = new Fraction(3, 4);    // 3/4
+        Fraction fraction4 = new Fraction(1, 3);    // 1/3
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+    }
+}
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom;
+    }
+
+    public int Numerator
+    {
+        get { return numerator; }
+        set { numerator = value; }
+    }
+
+    public int Denominator
+    {
+        get { return denominator; }
+        set
+        {
+            if (value != 0)
+                denominator = value;
+            else
+                Console.WriteLine("Denominator cannot be zero.");
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
     }
 }
