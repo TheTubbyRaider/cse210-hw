335545059c9a3c1abd57f315e46dc0d41becabe4
TheTubbyRaider <grsraider66@gmail.com>
2023-10-05 22:36:59 -0600

    week4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create an instance of MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+        // Call the GetSummary and GetHomeworkList methods and display the results
+        string mathSummary = mathAssignment.GetSummary();
+        string mathHomeworkList = mathAssignment.GetHomeworkList();
+
+        Console.WriteLine("Math Assignment:");
+        Console.WriteLine(mathSummary);
+        Console.WriteLine(mathHomeworkList);
+        Console.WriteLine();
+
+        // Create an instance of WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        // Call the GetSummary and GetWritingInformation methods and display the results
+        string writingSummary = writingAssignment.GetSummary();
+        string writingInformation = writingAssignment.GetWritingInformation();
+
+        Console.WriteLine("Writing Assignment:");
+        Console.WriteLine(writingSummary);
+        Console.WriteLine(writingInformation);
     }
 }
