68d1b2f6711afb9cbd3ffde4080e7e0cd2ab020d
TheTubbyRaider <grsraider66@gmail.com>
2023-10-24 18:22:37 -0600

    final

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,120 @@
 using System;
 
+class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public override string ToString()
+    {
+        return $"{street}, {city}, {state}, {country}";
+    }
+}
+
+class Event
+{
+    private string title;
+    private string description;
+    private string date;
+    private string time;
+    private Address address;
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    public string GetDetails()
+    {
+        return $"Title: {title}\nDescription: {description}\nDate: {date}\nTime: {time}\nAddress: {address}";
+    }
+
+    public virtual string GenerateMessage()
+    {
+        return GetDetails();
+    }
+}
+
+class Lecture : Event
+{
+    private string speaker;
+    private int capacity;
+
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    public override string GenerateMessage()
+    {
+        return base.GenerateMessage() + $"\nType: Lecture\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+}
+
+class Reception : Event
+{
+    private string rsvpEmail;
+
+    public Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GenerateMessage()
+    {
+        return base.GenerateMessage() + $"\nType: Reception\nRSVP Email: {rsvpEmail}";
+    }
+}
+
+class OutdoorGathering : Event
+{
+    private string weatherForecast;
+
+    public OutdoorGathering(string title, string description, string date, string time, Address address, string weatherForecast)
+        : base(title, description, date, time, address)
+    {
+        this.weatherForecast = weatherForecast;
+    }
+
+    public override string GenerateMessage()
+    {
+        return base.GenerateMessage() + $"\nType: Outdoor Gathering\nWeather Forecast: {weatherForecast}";
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address = new Address("123 Main St", "Cityville", "State", "Countryland");
+
+        Event lecture = new Lecture("Tech Talk", "A talk about technology", "2023-10-25", "10:00 AM", address, "John Doe", 100);
+        Event reception = new Reception("Party Time", "A fun evening", "2023-10-26", "6:00 PM", address, "rsvp@example.com");
+        Event outdoorGathering = new OutdoorGathering("Picnic", "Outdoor fun", "2023-10-27", "12:00 PM", address, "Sunny");
+
+        Event[] events = { lecture, reception, outdoorGathering };
+
+        foreach (Event e in events)
+        {
+            Console.WriteLine(e.GenerateMessage());
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,128 @@
 using System;
+using System.Collections.Generic;
+
+class Activity
+{
+    private string date;
+    protected int length; // Change 'private' to 'protected'
+
+    public Activity(string date, int length)
+    {
+        this.date = date;
+        this.length = length;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{date} {GetType().Name} ({length} min) - Distance: {GetDistance()}, Speed: {GetSpeed()}, Pace: {GetPace()}";
+    }
+}
+
+class Running : Activity
+{
+    private double distance;
+
+    public Running(string date, int length, double distance)
+        : base(date, length)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (distance / length) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return length / distance;
+    }
+}
+
+class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(string date, int length, double speed)
+        : base(date, length)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+}
+
+class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(string date, int length, int laps)
+        : base(date, length)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50 / 1000;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / length) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return length / GetDistance();
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        Running runningActivity = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cyclingActivity = new Cycling("03 Nov 2022", 30, 6.0);
+        Swimming swimmingActivity = new Swimming("03 Nov 2022", 30, 60);
+
+        activities.Add(runningActivity);
+        activities.Add(cyclingActivity);
+        activities.Add(swimmingActivity);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
